4fe27855efa86fa730cf845dfcc5addd237ffef1
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2024-05-19 11:01:10 +0300

    Extract member table into a separate component

--- app/components/team.tsx
+++ app/components/team.tsx
@@ -20,7 +20,7 @@ import ChevronDown from './icons/chevron-down';
 import EllipsisVertical from './icons/ellipsis-vertical';
 import Badge from './badge';
 import Dropdown from './dropdown';
-import Member from './member';
+import MemberTable from './member-table';
 import CreateTeamModal from './create-team-modal';
 import InviteMemberModal from './invite-member-modal';
 
@@ -204,38 +204,11 @@ export default function Team() {
                                 {selectedTeamId === team.id && (
                                     <tr>
                                         <td colSpan={6}>
-                                            <div className="m-4 w-full max-sm:overflow-x-auto max-sm:m-2">
-                                                <table className="w-full table-auto border-collapse text-left">
-                                                    <thead>
-                                                        <tr>
-                                                            <th className="p-4 text-gray-500 max-sm:p-2">
-                                                                <div
-                                                                    onClick={() => sortByTeamMember(selectedTeamId)}
-                                                                    className="flex cursor-pointer"
-                                                                >
-                                                                    Team member
-                                                                    <ChevronUpDown />
-                                                                </div>
-                                                            </th>
-                                                            <th className="p-4 text-gray-500 max-sm:p-2">Role</th>
-                                                            <th className="p-4 text-gray-500 text-nowrap max-sm:p-2">Product features</th>
-                                                            <th className="p-4 text-gray-500 max-sm:p-2">Accounts</th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                        {team.members.map((member: Member) => (
-                                                            <Member
-                                                                key={member.email}
-                                                                name={member.name}
-                                                                email={member.email}
-                                                                role={member.role}
-                                                                accounts={member.accounts}
-                                                                isPending={member.isPending}
-                                                            />
-                                                        ))}
-                                                    </tbody>
-                                                </table>
-                                            </div>
+                                            <MemberTable
+                                                teamId={selectedTeamId}
+                                                sortByTeamMember={sortByTeamMember}
+                                                members={team.members}
+                                            />
                                         </td>
                                     </tr>
                                 )}
