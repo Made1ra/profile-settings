ef844961293cfa727420baffeb0545f3c785136f
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2024-05-24 10:31:05 +0300

    Fix border

--- app/page.tsx
+++ app/page.tsx
@@ -12,19 +12,20 @@ export default function ProfileSettings() {
       <div className="w-full max-w-6xl">
         <h1 className="text-3xl font-bold mb-4">Settings</h1>
         <div className="bg-white shadow-xl rounded-xl">
-          <div className="border-b p-4 h-14">
+          <div className="p-4 h-12">
             <button
               onClick={() => setActiveTab('My account')}
-              className={`px-1 h-10 mr-4 text-lg ${activeTab === 'My account' ? 'text-black border-b-2 border-blue-400' : 'text-gray-500'}`}
+              className={`px-1 h-12 mr-4 text-lg ${activeTab === 'My account' ? 'text-black border-b-2 border-blue-400' : 'text-gray-500'}`}
             >
               My account
             </button>
             <button
               onClick={() => setActiveTab('Team')}
-              className={`px-1 h-10 text-lg ${activeTab === 'Team' ? 'text-black border-b-2 border-blue-400' : 'text-gray-500'}`}
+              className={`px-1 h-12 text-lg ${activeTab === 'Team' ? 'text-black border-b-2 border-blue-400' : 'text-gray-500'}`}
             >
               Team
             </button>
+            <div className="border-b" />
           </div>
           <div className="p-4">
             {activeTab === 'My account' && <MyAccount />}
